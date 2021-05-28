4b6877337e32d24c000e8862296fb6bfdd5584df
Yauhen Filipau <6479556@gmail.com>
2021-05-29 00:55:13 +0300

    add VKODS

--- src/main/java/getApiEgr/JsonSimpleParser.java
+++ src/main/java/getApiEgr/JsonSimpleParser.java
@@ -14,9 +14,9 @@ public class JsonSimpleParser {
         JSONParser parser = new JSONParser();
         try (FileReader reader = new FileReader("src/main/java/getApiEgr/test2.json")) {
             JSONObject rowJsonObject = (JSONObject) parser.parse(reader);
-            JSONObject rowJsonObject2 = (JSONObject) rowJsonObject.get("ROW");
-            String name = rowJsonObject2.get("VNAIMK").toString();
-            row.getOrganization().setVNAIMK(name);
+            JSONObject allFieldsOfOrganization = (JSONObject) rowJsonObject.get("ROW");
+            row.getOrganization().setVNAIMK(allFieldsOfOrganization.get("VNAIMK").toString());
+            row.getOrganization().setVKODS(allFieldsOfOrganization.get("VKODS").toString());
             return row;
         } catch (Exception e) {
             System.out.println("Error parsing: " + e.toString());
