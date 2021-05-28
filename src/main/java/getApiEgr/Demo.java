e4fefedc1e9228bfc37b5693602d147703124a34
Yauhen Filipau <6479556@gmail.com>
2021-05-28 18:50:35 +0300

    test with organization

--- src/main/java/getApiEgr/Demo.java
+++ src/main/java/getApiEgr/Demo.java
@@ -1,11 +1,11 @@
 package getApiEgr;
 
-import getApiEgr.model.Root;
+import getApiEgr.model.Row;
 
 public class Demo {
     public static void main(String[] args) {
         JsonSimpleParser jsonSimpleParser = new JsonSimpleParser();
-        Root root = jsonSimpleParser.parse();
-        System.out.println("Root " + root.toString());
+        Row row = jsonSimpleParser.parse();
+        System.out.println("Наименование: " + row.getOrganization().getVNAIMP());
     }
 }
