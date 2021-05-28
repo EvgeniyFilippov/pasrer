6a2367ad2d21f7e22e2d8b0d90e01ca601673530
Yauhen Filipau <6479556@gmail.com>
2021-05-28 18:51:15 +0300

    add Row and Organization

--- src/main/java/getApiEgr/JsonSimpleParser.java
+++ src/main/java/getApiEgr/JsonSimpleParser.java
@@ -1,19 +1,22 @@
 package getApiEgr;
 
+import getApiEgr.model.Organization;
 import getApiEgr.model.Root;
+import getApiEgr.model.Row;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import java.io.FileReader;
 
 public class JsonSimpleParser {
-    public Root parse() {
-        Root root = new Root();
+    public Row parse() {
+        Organization organization = new Organization();
+        Row row = new Row(organization);
         JSONParser parser = new JSONParser();
-        try (FileReader reader = new FileReader("src/main/java/getApiEgr/JsonParser.java")) {
-            JSONObject rootJsonObject = (JSONObject) parser.parse(reader);
-            String name = rootJsonObject.get("name").toString();
-            root.setName(name);
-            return root;
+        try (FileReader reader = new FileReader("src/main/java/getApiEgr/test2.json")) {
+            JSONObject rowJsonObject = (JSONObject) parser.parse(reader);
+            String name = rowJsonObject.get("ROW").toString();
+            row.getOrganization().setVNAIMK(name);
+            return row;
         } catch (Exception e) {
             System.out.println("Error parsing: " + e.toString());
         }
