6496967e5de4e285a378bbaf870097d7705b79a2
Yauhen Filipau <6479556@gmail.com>
2021-05-29 00:55:02 +0300

    add VKODS

--- src/main/java/getApiEgr/Demo.java
+++ src/main/java/getApiEgr/Demo.java
@@ -6,6 +6,6 @@ public class Demo {
     public static void main(String[] args) {
         JsonSimpleParser jsonSimpleParser = new JsonSimpleParser();
         Row row = jsonSimpleParser.parse();
-        System.out.println("Наименование: " + row.getOrganization().getVNAIMK());
+        System.out.println("Наименование: " + row.getOrganization().getVNAIMK() + "\nСтатус: " + row.getOrganization().getVKODS() );
     }
 }
