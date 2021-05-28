7d8b3489b58fcd4b0896cef7f981722b1974d24e
Yauhen Filipau <6479556@gmail.com>
2021-05-29 00:40:42 +0300

    test VNAIMK

--- src/main/java/getApiEgr/Demo.java
+++ src/main/java/getApiEgr/Demo.java
@@ -6,6 +6,6 @@ public class Demo {
     public static void main(String[] args) {
         JsonSimpleParser jsonSimpleParser = new JsonSimpleParser();
         Row row = jsonSimpleParser.parse();
-        System.out.println("Наименование: " + row.getOrganization().getVNAIMP());
+        System.out.println("Наименование: " + row.getOrganization().getVNAIMK());
     }
 }
