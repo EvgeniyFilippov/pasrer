ec10dab6898b56317bbf354b1f0f2e8bf8d4e7a0
Yauhen Filipau <6479556@gmail.com>
2021-05-29 15:28:28 +0300

    add parseFromUrl method

--- src/main/java/getApiEgr/JsonSimpleParser.java
+++ src/main/java/getApiEgr/JsonSimpleParser.java
@@ -5,7 +5,12 @@ import getApiEgr.model.Root;
 import getApiEgr.model.Row;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 public class JsonSimpleParser {
     public Row parse() {
@@ -20,6 +25,16 @@ public class JsonSimpleParser {
             return row;
         } catch (Exception e) {
             System.out.println("Error parsing: " + e.toString());
+        }
+        return null;
+    }
+
+    public Row parseFromUrl(URL url) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()))) {
+
+        } catch (IOException e) {
+            System.out.println("Error get info from URL. " + e.getMessage());
         }
         return null;
     }
