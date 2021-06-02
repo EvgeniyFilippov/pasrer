d079deab996dc030f0c14205559b66596f4ad431
Yauhen Filipau <6479556@gmail.com>
2021-06-02 22:45:41 +0300

    add test file

---
+++ src/test/java/getApiEgr/JsonSimpleParserTest.java
@@ -0,0 +1,15 @@
+package getApiEgr;
+
+import junit.framework.TestCase;
+
+public class JsonSimpleParserTest extends TestCase {
+
+    public void testParseFromUrl() {
+    }
+
+    public void testFillingRow() {
+    }
+
+    public void testGetValueFromJsonObject() {
+    }
+}
