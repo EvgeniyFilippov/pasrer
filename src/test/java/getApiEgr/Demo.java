a9a79633d2fa08e122cfe50767b85f47e8e53148
Yauhen Filipau <6479556@gmail.com>
2021-06-03 23:08:50 +0300

    add unp in signature

--- src/test/java/getApiEgr/Demo.java
+++ src/test/java/getApiEgr/Demo.java
@@ -12,13 +12,7 @@ public class Demo {
 //        System.out.println("Наименование: " + row.getOrganization().getShortName() + "\nСтатус: " + row.getOrganization().getState() );
 
         JsonSimpleParser jsonSimpleParserUrl = new JsonSimpleParser();
-        URL url = null;
-        try {
-            url = new URL("http://www.portal.nalog.gov.by/grp/getData?unp=192945049&charset=UTF-8&type=json");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        JSONObject finishJsonObject = jsonSimpleParserUrl.parseFromUrl(url);
+        JSONObject finishJsonObject = jsonSimpleParserUrl.parseFromUrl(192945049);
         Row row = jsonSimpleParserUrl.fillingRow(finishJsonObject);
 
         System.out.printf("Краткое наименование: %s\nПолное наименование: %s\nСтатус: %s\nУНП: %s\nДата изменения состояния: %s", row.getOrganization().getShortName(), row.getOrganization().getFullName(), row.getOrganization().getState(), row.getOrganization().getUnp(), row.getOrganization().getDateOfChange());
