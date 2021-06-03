3c9c29635b9922ad11a5d59e489726cc121f72d9
Yauhen Filipau <6479556@gmail.com>
2021-06-03 23:09:21 +0300

    add unp in signature method

--- src/test/java/getApiEgr/JsonSimpleParserTest.java
+++ src/test/java/getApiEgr/JsonSimpleParserTest.java
@@ -8,19 +8,14 @@ public class JsonSimpleParserTest extends TestCase {
 
     public void testParseFromUrl() {
         JsonSimpleParser jsonSimpleParserUrl = new JsonSimpleParser();
-        URL url = null;
-        try {
-            url = new URL("http://www.portal.nalog.gov.by/grp/getData?unp=192945049&charset=UTF-8&type=json");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+
         String expected = "{\"DREG\":\"25.07.2017\",\"NMNS\":\"107\",\"VLIKV\":null,\"VNAIMK\":\"ООО \\\"" +
                 "Спорт Витал\\\"\",\"VMNS\":\"Инспекция МНС по Первомайскому району г.Минска \",\"VKODS\":" +
                 "\"Действующий\",\"VUNP\":\"192945049\",\"VNAIMP\":\"Общество с ограниченной ответственностью " +
                 "\\\"Спорт Витал\\\"\",\"VPADRES\":\"г. Минск,ул. Городецкая, д.44, пом. 155А\",\"CKODSOST\":\"1" +
                 "\",\"DLIKV\":null}";
 
-        String actual = jsonSimpleParserUrl.parseFromUrl(url).toString();
+        String actual = jsonSimpleParserUrl.parseFromUrl(192945049).toString();
         assertEquals(expected, actual);
 
     }
