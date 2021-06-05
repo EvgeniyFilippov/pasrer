4c255fb7b9b8d93f1d33e00900093ce8df6be53a
Yauhen Filipau <6479556@gmail.com>
2021-06-05 14:23:07 +0300

    add color for state

--- src/test/java/getApiEgr/Demo.java
+++ src/test/java/getApiEgr/Demo.java
@@ -1,6 +1,7 @@
 package getApiEgr;
 
 import getApiEgr.model.Row;
+import getApiEgr.util.AnsiColorCode;
 import org.json.simple.JSONObject;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -14,6 +15,14 @@ public class Demo {
         JsonSimpleParser jsonSimpleParserUrl = new JsonSimpleParser();
         JSONObject finishJsonObject = jsonSimpleParserUrl.parseFromUrl(192945049);
         Row row = jsonSimpleParserUrl.fillingRow(finishJsonObject);
-        System.out.printf("Краткое наименование: %s\nПолное наименование: %s\nСтатус: %s\nУНП: %s\nДата изменения состояния: %s", row.getOrganization().getShortName(), row.getOrganization().getFullName(), row.getOrganization().getState(), row.getOrganization().getUnp(), row.getOrganization().getDateOfChange());
+        System.out.println("Краткое наименование: " + row.getOrganization().getShortName());
+        if (row.getOrganization().getState().equals("Действующий")) {
+            System.out.println(AnsiColorCode.FG_GREEN_BRIGHT + row.getOrganization().getState() + AnsiColorCode.RESET);
+        } else {
+            System.out.println(AnsiColorCode.FG_RED_BOLD_BRIGHT + row.getOrganization().getState() + AnsiColorCode.RESET);
+        }
+        System.out.printf("Полное наименование: %s\nСтатус: %s\nУНП: %s\nДата изменения состояния: %s",
+                row.getOrganization().getFullName(), row.getOrganization().getState(),
+                row.getOrganization().getUnp(), row.getOrganization().getDateOfChange());
     }
 }
